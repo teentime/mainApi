d3125de1c3de04f28ad6dbfb23a86e92040d916e
koldin108902 <alex108902@naver.com>
2023-06-02 06:18:35 +0900

    init

---
+++ src/main/java/kr/teentime/mainApi/MainApiApplication.java
@@ -0,0 +1,13 @@
+package kr.teentime.mainApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApiApplication.class, args);
+	}
+
+}
