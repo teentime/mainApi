6076cca5a5221c7bae877195549768a165cc8bdd
koldin108902 <alex108902@naver.com>
2023-06-08 10:37:07 +0900

    fixed: jwt filter 코드 추가 *doFilter하기*

--- src/main/java/kr/teentime/mainApi/config/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/kr/teentime/mainApi/config/security/jwt/JwtAuthenticationFilter.java
@@ -1,10 +1,14 @@
-package kr.teentime.mainApi.config.security;
+package kr.teentime.mainApi.config.security.jwt;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import kr.teentime.mainApi.config.security.jwt.JwtConfig;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -12,10 +16,21 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private final JwtConfig jwtConfig;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String header = request.getHeader("X-Auth-Token");
+        String token = jwtConfig.resolveToken(request);
 
+        if (token != null && jwtConfig.validateToken(token)) {
+            try {
+                Authentication authentication = jwtConfig.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (UsernameNotFoundException e) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "member not exist");
+            }
+        }
 
+        filterChain.doFilter(request, response);
     }
 }
