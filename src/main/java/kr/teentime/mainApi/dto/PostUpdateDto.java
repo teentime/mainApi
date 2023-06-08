c45f520c086cce63e4fb99b048b0bd53dc5f6443
koldin108902 <alex108902@naver.com>
2023-06-09 06:07:07 +0900

    Fix: dto extends 취소

--- src/main/java/kr/teentime/mainApi/dto/PostUpdateDto.java
+++ src/main/java/kr/teentime/mainApi/dto/PostUpdateDto.java
@@ -1,16 +1,28 @@
 package kr.teentime.mainApi.dto;
 
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
+import java.util.List;
+
+@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class PostUpdateDto extends PostWriteDto {
+public class PostUpdateDto {
 
+    @NotNull
     private Long postId;
+    @NotEmpty
+    private String title;
+    @NotEmpty
+    private String content;
 
-    public Long getPostId() {
+    private List<String> tags;
+
+    public @NotNull Long getPostId() {
         return postId;
     }
 }
