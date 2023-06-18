4a981a4e7125e5618391dee37bbeb4c646ab96b2
koldin108902 <alex108902@naver.com>
2023-06-19 04:32:21 +0900

    Feat: club admin 추가 기능

--- src/main/java/kr/teentime/mainApi/controller/ClubController.java
+++ src/main/java/kr/teentime/mainApi/controller/ClubController.java
@@ -1,10 +1,13 @@
 package kr.teentime.mainApi.controller;
 
 import jakarta.validation.Valid;
+import kr.teentime.mainApi.dto.admin.AddAdminDto;
 import kr.teentime.mainApi.dto.club.AddClubDto;
+import kr.teentime.mainApi.exception.ClubNotFoundException;
 import kr.teentime.mainApi.service.ClubService;
 import kr.teentime.mainApi.util.Result;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +23,18 @@ public class ClubController {
     public ResponseEntity addClub(@RequestBody @Valid AddClubDto addClubDto) {
 
         clubService.addClub(addClubDto);
-        return Result.ok("add club is successfully");
+        return Result.ok("add club is successfully done");
+    }
+
+    @PostMapping("/club/admin/add")
+    public ResponseEntity addAdmin(@RequestBody @Valid AddAdminDto addAdminDto) throws ClubNotFoundException {
+
+        try {
+            clubService.addAdmin(addAdminDto);
+            return Result.ok("add club admin is successfully done");
+        } catch (IllegalAccessException e) {
+            return Result.error(e.getMessage(), HttpStatus.FORBIDDEN.value());
+        }
     }
 
 }
