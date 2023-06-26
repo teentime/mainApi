9a74473a4508fd40ea3a805ee6124a6102545e7a
koldin108902 <alex108902@naver.com>
2023-06-27 04:46:28 +0900

    Fix: star에 null이 들어오는 경우 0으로 변환 삽입

--- src/main/java/kr/teentime/mainApi/dto/club/FindClubDto.java
+++ src/main/java/kr/teentime/mainApi/dto/club/FindClubDto.java
@@ -11,14 +11,16 @@ public class FindClubDto {
 
     private String name;
     private String intro;
-    private int star;
+    private double starAvg;
     private List<String> tags;
 
     @QueryProjection
-    public FindClubDto(String name, String intro, int star, String tags) {
+    public FindClubDto(String name, String intro, Double star, String tags) {
         this.name = name;
         this.intro = intro;
-        this.star = star;
+
+        // 평점이 0인 경우 0점으로 처리
+        this.starAvg = star == null ? 0 : star;
 
         // tags가 string으로 오는 것을 list 형으로 변환
         String str = tags.substring(1, tags.length() - 1); // 대괄호 제거
