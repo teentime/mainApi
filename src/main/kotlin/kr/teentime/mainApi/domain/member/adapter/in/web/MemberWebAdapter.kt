e4ec36e1b03c151bd193ab895fc6bdb18429eaaa
koldin108902 <alex108902@naver.com>
2023-08-17 10:52:10 +0900

    Fix: RequestMapping -> PostMapping

--- src/main/kotlin/kr/teentime/mainApi/domain/member/adapter/in/web/MemberWebAdapter.kt
+++ src/main/kotlin/kr/teentime/mainApi/domain/member/adapter/in/web/MemberWebAdapter.kt
@@ -7,6 +7,7 @@ import kr.teentime.mainApi.domain.member.port.`in`.SearchMemberByIdUseCase
 import kr.teentime.mainApi.global.error.ErrorCode
 import kr.teentime.mainApi.global.response.BasicResponse
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -19,7 +20,7 @@ class MemberWebAdapter(
     private val searchMemberByIdUseCase: SearchMemberByIdUseCase,
 ) {
 
-    @RequestMapping("/join")
+    @PostMapping("/join")
     fun join(@RequestBody joinDto: SaveMemberRequest) {
         try {
             val saveMember = saveMemberUseCase.execute(joinDto)
