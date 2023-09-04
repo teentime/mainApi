34479c8c7c705002abe8a896616e88563e3fb7b8
koldin108902 <alex108902@naver.com>
2023-09-04 10:09:27 +0900

    Feat: preflight 요청 통과 설정

--- src/main/kotlin/kr/teentime/mainApi/global/security/SecurityConfig.kt
+++ src/main/kotlin/kr/teentime/mainApi/global/security/SecurityConfig.kt
@@ -3,7 +3,6 @@ package kr.teentime.mainApi.global.security
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -11,6 +10,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.util.matcher.RequestMatcher
+import org.springframework.web.cors.CorsUtils
 
 
 @Configuration
@@ -23,8 +24,10 @@ open class SecurityConfig {
             .cors { it.disable() }
             .csrf { it.disable() }
             .authorizeHttpRequests {
-                it.anyRequest()
-                    .permitAll()
+                it.anyRequest().permitAll()
+                    .requestMatchers(RequestMatcher { req ->
+                        CorsUtils.isPreFlightRequest(req)
+                    }).permitAll()
             }
             .exceptionHandling {
                 it.authenticationEntryPoint(
